75145805a2aacfab27dfc2bccdd33b062132a6b1
Noah Henok <henok1976@gmail.com>
2024-02-19 12:01:41 -0500

    I'm restarting

--- dad.js
+++
@@ -1,5 +0,0 @@
-const fn_pic_hide = document.querySelector("#pic")
-
-fn_pic_hide.addEventListener("click", () => {
-    pagePic.classList.toggle("hidden");
-});
--- nathan.js
+++
@@ -1,5 +0,0 @@
-const fn_pic_pusle = document.querySelector(".pulse");
-
-fn_pic_pusle.addEventListener("click", () => {
-    pagePic.classList.toggle("hidden");
-});
